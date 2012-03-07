8673f2736a5ca99002a35f8ff490cdc9a1bf58a5
nguyentoan1212 <nguyentoan1212@0a700d09-01b6-0b21-7a7a-7f6f4cd4cb47>
2012-03-07 03:25:49 +0000

    config/routes.php va tao admin_index.ctp

--- config/routes.php
+++ config/routes.php
@@ -33,3 +33,4 @@
  * ...and connect the rest of 'Pages' controller's urls.
  */
 	Router::connect('/pages/*', array('controller' => 'pages', 'action' => 'display'));
+	Router::connect('/admin', array('admin'=> true,'controller' => 'users', 'action' => 'index'));
