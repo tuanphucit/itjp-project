511667879ee477ada10b3b3a07c8a904077d547d
nguyentoan1212 <nguyentoan1212@0a700d09-01b6-0b21-7a7a-7f6f4cd4cb47>
2012-03-11 13:38:43 +0000

    them admin

--- trunk/controllers/users_controller.php
+++ trunk/controllers/users_controller.php
@@ -4,7 +4,7 @@ class UsersController extends AppController {
 	var $name = 'Users';
 	var $helpers = array('Ajax', 'Javascript');
 	var $uses = array('Company', 'User');
-	
+	var $_limit = 3;
 	function beforeFilter(){
 		$this->Auth->allow ( 'register', 'confirm', 'forgotpswd', 'reset' );
 		$this->Auth->fields = array ('username' => 'email', 'password' => 'password' );
@@ -141,7 +141,7 @@ class UsersController extends AppController {
 		$this->set ( 'user', $this->data );
 	}
 	
-	//admin
+	/*
 	function admin_index() {
 		$this->User->recursive = 0;
 		$this->set('users', $this->paginate());
@@ -154,7 +154,214 @@ class UsersController extends AppController {
 		}
 		$this->set('user', $this->User->read(null, $id));
 	}
+	*/
+	function admin_login() {
 
+		//		'recursive' => - 1,
+		$this->layout = 'admin_login';
+		if (! empty ( $this->data )) {
+			if (isset ( $this->data ['User'] ['email'] )) {
+				$user = &$this->User->find ( 'first', array ('conditions' => array (
+					'User.email' => $this->data ['User'] ['email'],
+					'User.role' => 0 ), 
+					 'fields' => array ('User.role' ) ) );
+				if (! empty ( $user ) && ($user ['User'] ['role'] == 0)) {
+					if ($this->Auth->login ( $this->data )) {
+						$this->redirect ( $this->referer () );
+						//$this->redirect('website');
+					} else {
+						$this->Session->setFlash ( __ ( 'Email or password is invalid.', true ) );
+
+					}
+				} else {
+					$this->Session->setFlash ( "You don't accept permission to login admin.");
+					$this->redirect ( 'index' );
+				}
+			} else {
+				$this->flash ( "Your account haven't been found.Please try again.", $this->here );
+			}
+
+		}
+		//debug($this->data);
+
+	}
+	
+	
+	function admin_index() {
+		$this->layout = "admin";
+		$page = 1;
+		if (!empty($this->data['show'])){
+			if ($this->data['show']!=0){
+				$this->_limit = $this->data['show'];
+			}
+			
+			
+			$this->data['User'] = $this->Session->read('search');
+			$this->set('show', $this->_limit);
+			$this->Session->write('show', $this->_limit);
+		}
+		else $this->set('show', $this->_limit);
+		
+		
+		$fields = array ('User.id', 'User.email', 'User.create_time', 'User.last_access', 'User.role');
+		$group = '';
+		if (! empty ( $this->params ['named'] ['page'] )) {
+			$page = $this->params ['named'] ['page'];
+			$this->data['User'] = $this->Session->read('search');
+			if (!empty($this->data['show']))
+				$this->_limit = $this->data['show'];
+			else 
+				$this->_limit = $this->Session->read('show');
+		}
+		
+		//$this->User->Website->unbindModel ( array ('belongsTo' => array ('Category' ), 'hasMany' => array ('Webpage' ) ) );
+		if (empty ( $this->data ) ) {
+			$this->set ( 'show', $this->_limit );
+			//$this->User->recursive = -1;
+			$conditions = array ();
+			$conditions ['User.role >'] = 0;
+			$conditions ['User.status >'] = -1;
+
+			$this->paginate = array ('conditions' => $conditions, 'limit' => $this->_limit );
+			$users = &$this->paginate ( 'User' );
+			$this->set ( 'users', $users );
+			$this->set ( 'data', 0 );
+			$this->Session->write ( 'result', $users );
+			
+			//session for $this->data
+			$this->Session->write ('search', $this->data['User']);
+		} else {
+			
+			
+			if (array_key_exists('User', $this->data)){
+			$from_year = $this->data ['User'] ['from'] ['year'];
+			$from_month = $this->data ['User'] ['from'] ['month'];
+			$from_day = $this->data ['User'] ['from'] ['day'];
+
+			$to_year = $this->data ['User'] ['to'] ['year'];
+			$to_month = $this->data ['User'] ['to'] ['month'];
+			$to_day = $this->data ['User'] ['to'] ['day'];
+
+			if (($from_day != null || $from_month != null) && $from_year == null) {
+				$this->Session->setFlash ( 'Please select a year of Register Date (from)' );
+			} else {
+				if ($from_day == null)
+				$from_day = '01';
+				if ($from_month == null)
+				$from_month = '01';
+			}
+
+			if (($to_day != null || $to_month != null) && $to_year == null) {
+				$this->Session->setFlash ( 'Please select a year of Register Date (to)' );
+			} else {
+				if ($to_day == null)
+				$to_day = '31';
+				if ($to_month == null)
+				$to_month = '12';
+			}
+			$from = $from_year . '-' . $from_month . '-' . $from_day . ' 00:00:00';
+			//debug($from);
+			$to = $to_year . '-' . $to_month . '-' . $to_day . ' 00:00:00';
+
+			$conditions = array ();
+
+			
+			if ($this->data ['User'] ['role'] != '') {
+				$conditions ['User.role'] = $this->data ['User'] ['role'];
+			}
+			if ($this->data ['User'] ['status'] != '') {
+				$conditions ['User.status'] = $this->data ['User'] ['status'];
+			}
+
+			if (strlen ( $from ) == 19) {
+				$conditions ['User.create_date >'] = $from;
+			}
+			if (strlen ( $to ) == 19) {
+				$conditions ['User.create_date <'] = $to;
+			}
+			if ($this->data ['User'] ['email'] != '') {
+				$conditions ['User.email LIKE'] = "%" . $this->data ['User'] ['email'] . "%";
+			}
+			$conditions ['User.role >'] = 0;
+			$conditions ['User.status >'] = -1;
+			$this->set ( 'data', 0 );
+
+			if ($this->data ['User'] ['website_count'] != '') {
+				$this->log ( 'co website count' . $this->data ['User'] ['website_count'] . 'test', 'abc' );
+				$group = 'Website.user_id HAVING count = ' . $this->data ['User'] ['website_count'];
+				$this->paginate = array ('fields' => $fields, 'conditions' => $conditions, 'group' => $group, 'limit' => $this->_limit );
+				$this->set ( 'data', 1 );
+
+				$this->set ( 'users', $this->paginate () );
+				$this->Session->write ( 'result', $this->paginate () );
+				
+				$this->Session->write ('search', $this->data['User']);
+			}
+
+			else {
+				$this->paginate = array ('conditions' => $conditions, 'limit' => $this->_limit );
+				$this->set ( 'users', $this->paginate ( 'User' ) );
+				$this->Session->write ( 'result', $this->paginate ( 'User' ) );
+				
+				$this->Session->write ('search', $this->data['User']);
+			}
+			}
+		}
+		
+		$this->set ( 'page', $page );
+		$this->set ( 'limit', $this->_limit );
+		$page = $this->Session->write('page', $page);
+		//debug($this->data);
+	}
+
+	function admin_view($id = null) {
+		if (! $id) {
+			$this->Session->setFlash ( __ ( 'Invalid user', true ) );
+			$this->redirect ( array ('action' => 'index' ) );
+		}
+		$this->set ( 'user', $this->User->read ( null, $id ) );
+
+
+		$page=1;
+		if (! empty ( $this->params ['named'] ['page'] )) {
+			$page = $this->params ['named'] ['page'];
+		}
+		$this->set ( 'page', $page );
+		$this->set ( 'limit', $this->_limit );
+		$this->set('numWebs', count($this->data['Website']) );
+
+		$this->paginate = array('conditions'=>array('Website.user_id'=>$id, 'Website.status >'=> -1));
+		$this->set('websites', $this->paginate('Website'));
+
+	}
+
+	function admin_add() {
+
+		if (! empty ( $this->data )) {
+			$this->User->create ();
+			if (Validation::email($this->data['User']['email'])){
+			if ($this->data ['User'] ['password']!==$this->Auth->password ('')){
+				$confirm = $this->Auth->password ( $this->data ['User'] ['confirm'] );
+				if ($this->data ['User'] ['password'] == $confirm) {
+					$this->data ['User'] ['create_date'] = date ( 'Y-m-d H:m:s' );
+					$this->data ['User'] ['last_update'] = date ( 'Y-m-d H:m:s' );
+					$this->data ['User'] ['newsletter_flag'] = 1;
+					if ($this->User->save ( $this->data )) {
+						$this->Session->setFlash ( __ ( 'The user has been saved', true ) );
+						$this->redirect ( array ('action' => 'index' ) );
+					}
+				} else
+				$this->Session->setFlash ( __ ( 'Wrong of your password confirm. Please try again!', true ) );
+		
+			}
+			else $this->Session->setFlash ( __ ( 'Password must not be empty!', true ) );
+		}else  $this->Session->setFlash ( __ ( 'Email must be valid format!', true ) );
+		}
+		
+
+
+	}
+	/*
 	function admin_add() {
 		if (!empty($this->data)) {
 			$this->User->create();
@@ -184,7 +391,7 @@ class UsersController extends AppController {
 			$this->data = $this->User->read(null, $id);
 		}
 	}
-
+*/
 	function admin_delete($id = null) {
 		if (!$id) {
 			$this->Session->setFlash(__('Invalid id for user', true));
--- trunk/views/users/admin_index.ctp
+++ trunk/views/users/admin_index.ctp
@@ -1,57 +1,198 @@
-<div class="users index">
-	<h2><?php __('Users');?></h2>
+<div id="content">
+	<div id="tab_title">
+		<div id="tit">User Management</div>
+		
+		<div id="csv2">
+			<?php $url=array('controller'=>'users', 'action'=>'#');?>
+			<form method="post" 
+			action="<?php echo $this->Html->url($url);?>">
+			<input type="submit" value="CSV Export"/>
+			</form>
+		</div>
+		<div id="csv">
+			<?php $url=array('controller'=>'users', 'action'=>'#');?>
+			<form enctype="multipart/form-data" method="post" 
+			action="<?php echo $this->Html->url($url);?>">
+			Import CSV
+			<input type="file" name="file" />
+			<input type="submit" value="Upload" />
+			</form>
+		</div>
+		
+		
+
+	</div>
+	
+	<div id="form_box">
+	
+	<?php echo $this->Form->create('User', array('action'=>'admin_index', 'method'=>'post'));?>
+	<p class="pad">
+		<?php echo __('Status');?>
+		<?php $option=array(''=>'Select One',
+							'1'=>'Active', 
+							'0'=>'Disable',
+							'2'=>'Registered'
+		);?>
+		<?php echo $this->Form->select('status', $option, null, array('empty'=>'-Select-'));?>
+		
+		<p class="pad">
+		<?php  echo __('Register Date:'); ?> 
+		Y <?php echo $this->Form->year('from', 2012, 2050, null, array('empty'=>'- - - -'));?>
+		M <?php echo $this->Form->month('from',null, array('monthNames'=>false, 'empty'=>'- -'));?>
+		D <?php echo $this->Form->day('from',null, array('empty'=>'- -'));?> 
+		~ 
+		Y <?php echo $this->Form->year('to', 2012, 2050, null, array('empty'=>'- - - -'));?>
+		M <?php echo $this->Form->month('to', null, array('monthNames'=>false, 'empty'=>'- -'));?>
+		D <?php echo $this->Form->day('to', null, array('empty'=>'- -'));?> 
+		
+		</p>
+		
+		<p>
+		Text search 
+		<?php echo $this->Form->text('email', array('br'=>false)); ?>
+		<?php echo $this->Form->end(array('label'=>'Refresh and View', 'div'=>false));?>
+		
+		</p>
+	
+		
+	</div>
+
+	<div id="user_add">
+		<?php echo $this->Html->link('Add user', array('controller'=>'users', 'action'=>'admin_add'));?>
+		<?php echo $this->Html->image('user_add.png', array('width'=>15, 'border'=>0, 
+		));?>
+		
+	</div>
+	
+	
+	<div>
+	
+	<div id="user">
+	<?php $url=array('controller'=>'users', 'action'=>'admin_action');?>
+	<form name="form1" action="<?php echo $this->Html->url($url);?>" method="post">
+	<div id="submit_div">
+	
+		<?php $option=array('1'=>'Active', '2'=>'Disable', '3'=>'Delete', '4'=>'Send Mail');
+		//echo $this->Form->create('User', array('action'=>'admin_action', 'name'=>'form1','method'=>'post'));
+		echo $this->Form->select('User.action', $option, null, array('empty'=>'-Select-'));?>
+		<input type="submit" value="Submit" onclick="return submitUserAction(form1);">
+		
+		<span style="text-align: right; color: white; float: right; padding-top:5px; "><?php echo $this->Paginator->counter(array(
+		'format' => __('Result %current% records of %count%', true)
+		));?></span>
+
+	</div>
+	
+
+	
 	<table cellpadding="0" cellspacing="0">
+	
 	<tr>
 			<th><?php echo __('#');?></th>
-			<th><?php echo $this->Paginator->sort('usercode');?></th>
-			<th><?php echo $this->Paginator->sort('fullname');?></th>
-			<th><?php echo $this->Paginator->sort('company');?></th>
-			<th><?php echo $this->Paginator->sort('email');?></th>
-			<th><?php echo $this->Paginator->sort('phone');?></th>
-			<th><?php echo $this->Paginator->sort('local_phone');?></th>
-			<th class="actions"><?php __('Actions');?></th>
+			<th><?php echo $this->Form->checkbox('allbox', array('onclick'=>'checkAll()'));?></th>
+			<th><?php echo $this->Paginator->sort('User', 'email');?></th>
+			
+			<th><?php echo $this->Paginator->sort('Created','created_time');?></th>
+			
+			<th><?php echo $this->Paginator->sort('last_access');?></th>
+			<th><?php echo $this->Paginator->sort('status');?></th>
+			<th><?php echo $this->Paginator->sort('Critical', 'ws_critical');?></th>
+			<th><?php echo __('No of booking');?></th>
+			<th><?php echo __('Edit');?></th>
+			
+			
 	</tr>
 	<?php
 	$i = 0;
+	$row=0;
+	$index=0;
 	foreach ($users as $user):
 		$class = null;
 		if ($i++ % 2 == 0) {
 			$class = ' class="altrow"';
 		}
+		$type_id=$user['User']['role'];
+		$status_id=$user['User']['status'];
+		if ($type_id==0)
+			continue;
 	?>
 	<tr<?php echo $class;?>>
-		<td><?php echo $i; ?>&nbsp;</td>
-		<td><?php echo $user['User']['usercode']; ?>&nbsp;</td>
-		<td><?php echo $user['User']['fullname']; ?>&nbsp;</td>
-		<td><?php echo $user['User']['company']; ?>&nbsp;</td>
-		<td><?php echo $user['User']['email']; ?>&nbsp;</td>
-		<td><?php echo $user['User']['phone']; ?>&nbsp;</td>
-		<td><?php echo $user['User']['local_phone']; ?>&nbsp;</td>
-		<td class="actions">
-			<?php echo $this->Html->link(__('View', true), array('action' => 'view', $user['User']['id'])); ?>
-			<?php echo $this->Html->link(__('Edit', true), array('action' => 'edit', $user['User']['id'])); ?>
-			<?php echo $this->Html->link(__('Delete', true), array('action' => 'delete', $user['User']['id']), null, sprintf(__('Are you sure you want to delete # %s?', true), $user['User']['id'])); ?>
+		<?php $id=$user['User']['id'];
+		$index++;
+		$row++;?>
+		<td><?php echo $row+($page-1)*$limit; ?>&nbsp;</td>
+		<td><?php echo $this->Form->checkbox('User.check.'.($index-1), array('value'=>$id));?></td>
+		<td class="email"><?php echo $this->Html->link($user['User']['email'], array('action'=>'view', $id)); ?>&nbsp;</td>
+		
+		<td><?php echo $user['User']['created_time']; ?>&nbsp;</td>
+		
+		<td><?php echo $user['User']['last_access']; ?>&nbsp;</td>
+		<td><?php
+			switch ($status_id){
+				case 2:
+					__('Registerd');
+					break;
+				case 1:{
+					__('Active');
+					break;
+				}
+				case 0:{
+					__('Disable');
+					break;
+				}
+			} 
+		?>&nbsp;</td>
+		
+		<td>
+			<?php
+			if (isset($user['User']['ws_critical'])){
+				$flag_id=$user['User']['ws_critical'];
+				if ($flag_id == 1)
+					$flag = 'Good';
+				elseif ($flag_id==0)
+					$flag = 'Normal';
+				else $flag = 'Bad';
+			}else $flag = 'Good/Normal/Bad';
+			echo $flag; 
+			?>
+			
 		</td>
+		
+		<td>Chua lam&nbsp;</td>
+		<td><?php echo $this->Html->image('user_edit.png', array('width'=>18, 'border'=>0, 
+		'url'=>array('controller'=>'users', 'action'=>'admin_edit', $id)));?></td>
+		
 	</tr>
 <?php endforeach; ?>
+	
+	
+	
 	</table>
-	<p>
-	<?php
-	echo $this->Paginator->counter(array(
-	'format' => __('Page %page% of %pages%, showing %current% records out of %count% total, starting on record %start%, ending on %end%', true)
-	));
-	?>	</p>
-
+	</form>
+	<div id="count">
+	</div>
+	
+	<div style="margin-top: 1em;">
+	<div class="show">
+		<?php $url=array('controller'=>'users', 'action'=>'admin_index');?>
+		<form action="<?php echo $this->Html->url($url);?>" method="post">
+		<?php echo __('Rows of a page: ', true);?>
+		<input type="text" name="data[show]" style="width: 40px;" 
+		value="<?php echo (isset($show) ? $show : '');?>" />
+		<input type="submit" value="Show">
+		</form>
+	</div>
 	<div class="paging">
 		<?php echo $this->Paginator->prev('<< ' . __('previous', true), array(), null, array('class'=>'disabled'));?>
 	 | 	<?php echo $this->Paginator->numbers();?>
  |
 		<?php echo $this->Paginator->next(__('next', true) . ' >>', array(), null, array('class' => 'disabled'));?>
 	</div>
+	
+	
+	</div>
 </div>
-<div class="actions">
-	<h3><?php __('Actions'); ?></h3>
-	<ul>
-		<li><?php echo $this->Html->link(__('New User', true), array('action' => 'add')); ?></li>
-	</ul>
+
 </div>
+</div>
+ 
