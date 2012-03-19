53cc71491ad1098aed5e46a126fd5b760eded7ce
nguyentoan1212 <nguyentoan1212@0a700d09-01b6-0b21-7a7a-7f6f4cd4cb47>
2012-03-19 04:47:14 +0000

    sua conflict equipment1

--- trunk/models/equipment1.php
+++ trunk/models/equipment1.php
@@ -1,16 +1,14 @@
 <?php 
 class Equipment1 extends AppModel{
 	var $name = 'Equipment1';
-<<<<<<< .mine
+	var $validate = array(
+	'code' => array('rule' => 'notEmpty','message' => 'Not null'),
+	'name' => array('rule' => 'notEmpty','message' => 'Not null'),
+	'description' => array('rule' => 'notEmpty','message' => 'Not null'),	
+	'price' => array( 'rule' => 'numeric', 'message' => 'Must be number'),
+	'quantity' => array('rule' => 'numeric', 'message' => 'Must be number')
+	);
 	
-		var $validate = array(
-		'code' => array('rule' => 'notEmpty','message' => 'Not null'),
-		'name' => array('rule' => 'notEmpty','message' => 'Not null'),
-		'description' => array('rule' => 'notEmpty','message' => 'Not null'),	
-		'price' => array( 'rule' => 'numeric', 'message' => 'Must be number'),
-		'quantity' => array('rule' => 'numeric', 'message' => 'Must be number')
-		);
-=======
 	var $hasMany = array(
 		'PositionsOfEquipment' => array(
 			'className' => 'PositionsOfEquipment',
@@ -23,6 +21,5 @@ class Equipment1 extends AppModel{
 			'counterQuery' => ''
 		)
 	);
->>>>>>> .r31
 }
 ?>
