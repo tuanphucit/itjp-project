e0264e55ea6ad97890f4f0114bfe8ce21d9ea408
ngocoanh.bk52@gmail.com <ngocoanh.bk52@gmail.com@0a700d09-01b6-0b21-7a7a-7f6f4cd4cb47>
2012-03-26 06:42:35 +0000

    them Const cho status request detail

--- trunk/config/constant.php
+++ trunk/config/constant.php
@@ -15,11 +15,18 @@ define('USER_WS_CRITICAL_GOOD', 1);
 
 //REQUEST
 define('REQUEST_STATUS_INIT', 0);
-define('REQUEST_STATUS_BOOKED', 1);
-define('REQUEST_STATUS_DENY', 2);
-define('REQUEST_STATUS_CANCEL_WAITING', 3);
+define('REQUEST_STATUS_APROVED', 1);
+define('REQUEST_STATUS_DENIED', 2);
+define('REQUEST_STATUS_HAS_UPDATED', 3);
 define('REQUEST_STATUS_CANCELED', 4); // Da huy phong
 define('REQUEST_STATUS_FINISH', 5); // Da qua ngay dang ky su dung
+
+define('REQUEST_DETAIL_STATUS_INIT', 0);
+define('REQUEST_DETAIL_STATUS_APROVED', 1);
+define('REQUEST_DETAIL_STATUS_DENIED', 2);
+define('REQUEST_DETAIL_STATUS_HAS_UPDATED', 3);
+define('REQUEST_DETAIL_STATUS_CANCELED', 4); // Da huy phong
+define('REQUEST_DETAIL_STATUS_FINISH', 5); // Da qua ngay dang ky su dung
 
 define('CLASS_ERROR_ALERT', 'alert_error');
 define('CLASS_INFO_ALERT', 'alert_info');
